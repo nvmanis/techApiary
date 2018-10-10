02ebbfb729b5e3d8fb99b974e86b62e70979822d
EricChavarria <VGCbolt@gmail.com>
2018-10-10 12:11:32 -0400

    Add Javascript

---
+++ js/article.js
@@ -0,0 +1,9 @@
+function Article(articleTitle, source, imageTitle, description){
+  this.articleTitle = articleTitle;
+  this.source = source;
+  this.imageTitle = imageTitle;
+  this.description = description;
+  this.outputArticle = function(){
+    document.write("<article class=article-content><h3>"+articleTitle+"</h3><img src="+source+" alt="+imageTitle+" title="+imageTitle+"/><p>"+description+"</p><a href="+""+">Read More</a></article>");
+  }
+}
---
+++ js/articles.js
@@ -0,0 +1,14 @@
+var articles = [];
+
+var article1 = new Article("The New iPhone X","https://placebear.com/400/200","Article Image","The iPhone X has been shooting off of the sheleves recently....");
+var article2 = new Article("The New iPhone X","https://placebear.com/400/200","Article Image","The iPhone X has been shooting off of the sheleves recently....");
+var article3 = new Article("The New iPhone X","https://placebear.com/400/200","Article Image","The iPhone X has been shooting off of the sheleves recently....");
+var article4 = new Article("The New iPhone X","https://placebear.com/400/200","Article Image","The iPhone X has been shooting off of the sheleves recently....");
+var article5 = new Article("The New iPhone X","https://placebear.com/400/200","Article Image","The iPhone X has been shooting off of the sheleves recently....");
+var article6 = new Article("The New iPhone X","https://placebear.com/400/200","Article Image","The iPhone X has been shooting off of the sheleves recently....");
+articles.push(article1);
+articles.push(article2);
+articles.push(article3);
+articles.push(article4);
+articles.push(article5);
+articles.push(article6);

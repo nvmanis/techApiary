9c79f9081596cfe0957058a7b056e50efa1a4943
EricChavarria <VGCbolt@gmail.com>
2018-11-19 18:08:10 -0500

    Finish adding form and styling it in article.php for comments. also changed all references of article.html to article.php to avoid any errors

--- html/article.php
+++ html/article.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" href="../css/article.css"/>
     <title>Article</title>
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-    <script type="text/javascript" src="../js/clickEvents.js"></script> 
+    <script type="text/javascript" src="../js/clickEvents.js"></script>
 </head>
 
 <body>
@@ -36,7 +36,7 @@
             </figure>
 
             <section id="text">
-                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> 
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </section>
@@ -52,12 +52,35 @@
                     <tr>
                             <td><p class="comment">"What about the notch tho"</p><p class="user">User: Danielle Doe</p></td>
                     </tr>
+                    <?php
+                      if(isset($_POST["name"])){
+                        $comment = $_POST["comment"];
+                        $name = $_POST["name"];
+                        echo("<tr><td><p class=comment>\"$comment\"</p><p class=user>User: $name</p></td></tr>");
+                      }
+                    ?>
             </table>
-       
-        </article>
-        
-    
 
-    <p id="copyright"><small>&copy Copyright TechAPIary 2018</small></p>
+        </article>
+
+        <div class="formDiv">
+
+        <h3 id="createComment">Add A Comment:</h3>
+        <form class="commentForm" action="article.php" method="post">
+          <div class="commentField">
+            <label for="name">Name: </label>
+            <input type="text" name="name" value="" required=""/>
+          </div>
+          <div class="commentField">
+            <label for="comment">Comment: </label>
+            <input type="text" name="comment" value="" required/>
+          </div>
+          <div class="commentField">
+            <input type='submit' value='Submit Comment' />
+          </div>
+        </form>
+
+          <p id="copyright"><small>&copy Copyright TechAPIary 2018</small></p>
+        </div>
 
 </body>
